2b12d1c381622384d57076cd16ca5d08e65000b0
Tatsuya1112 <45190789+Tatsuya1112@users.noreply.github.com>
2020-04-07 12:00:32 +0900

    Update train.py

--- train.py
+++ train.py
@@ -7,18 +7,15 @@ from tqdm import tqdm
 import torch.nn as nn
 import torch.optim as optim
 import argparse
-import pandas as pd
-from visdom import Visdom
 from preprocess import *
 from utils import *
 from loss import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--batch_size', default=2)
-parser.add_argument('--epochs', default=50)
+parser.add_argument('--epochs', default=30)
 parser.add_argument('--device', default=torch.device('cuda:0' if torch.cuda.is_available() else 'cpu'))
-parser.add_argument('--save_path', default='./finetuning.pth')
-parser.add_argument('--visualize', default=True)
+parser.add_argument('--save_path', default='./model.pth')
 parser.add_argument('--weight_decay', default=1e-4)
 parser.add_argument('--momentum', default=0.9)
 parser.add_argument('--is_aux', default=True)
@@ -27,9 +24,6 @@ parser.add_argument('--pretrained', default=True)
 parser.add_argument('--n_classes', default=21)
 
 args = parser.parse_args()
-
-if args.visualize:
-	vis = Visdom()
 
 print("-----------config")
 for arg in vars(args):
@@ -78,10 +72,6 @@ optimizer = optim.SGD([
 
 scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda_epoch)
 
-log = pd.DataFrame(index=[], columns=[
-    'epoch', 'train_loss', 'train_accuracy', 'test_loss', 'test_accuracy', 'train_iou', 'test_iou'
-])
-
 for epoch in range(args.epochs):
 
 	train_loss = AverageMeter()
@@ -103,9 +93,6 @@ for epoch in range(args.epochs):
 
 		if args.is_aux:
 			outputs = outputs["out"]
-
-		train_loss.update(loss.item(), args.batch_size)
-		train_accuracy.update(torch.sum(torch.max(outputs, dim=1)[1]==targets).type(torch.float).item()/(targets.shape[0]*targets.shape[1]*targets.shape[2]), args.batch_size)
 
 		predicts = torch.max(outputs, dim=1)[1]
 		for j in range(args.n_classes):
@@ -137,9 +124,6 @@ for epoch in range(args.epochs):
 			if args.is_aux:
 				outputs = outputs["out"]
 
-			test_loss.update(loss.item(), args.batch_size)
-			test_accuracy.update(torch.sum(torch.max(outputs, dim=1)[1]==targets).type(torch.float).item()/(targets.shape[0]*targets.shape[1]*targets.shape[2]), args.batch_size)
-
 			predicts = torch.max(outputs, dim=1)[1]
 			for j in range(args.n_classes):
 				test_tp[j]+=(torch.sum((predicts==j) & (targets==j)))
@@ -153,26 +137,5 @@ for epoch in range(args.epochs):
 
 
 	print('epoch: {:}, train_loss: {:.3f}, train_accuracy: {:.3f}, test_loss: {:.3f}, test_accuracy: {:.3f}, train_mIoU: {:.3f}, test_mIoU: {:.3f}'.format(epoch, train_loss.avg, train_accuracy.avg, test_loss.avg, test_accuracy.avg, train_iou, test_iou))
-
-	if args.visualize:
-		vis.line(X=np.array([epoch]), Y=np.array([train_loss.avg]), win="loss", name="train_loss", update="append", opts=dict(title="loss", legend=["train_loss", "test_loss"]))
-		vis.line(X=np.array([epoch]), Y=np.array([test_loss.avg]), win="loss", name="test_loss", update="append")
-		vis.line(X=np.array([epoch]), Y=np.array([train_accuracy.avg]), win='accuracy', name='train_accuracy', update="append", opts=dict(title="accuracy", legend=["train_accuracy", "test_accuracy"]))
-		vis.line(X=np.array([epoch]), Y=np.array([test_accuracy.avg]), win='accuracy', name='test_accuracy', update="append")
-		vis.line(X=np.array([epoch]), Y=np.array([train_iou]), win='mIoU', name='train_mIoU', update="append", opts=dict(title="mIoU", legend=["train_mIoU", "test_mIoU"]))
-		vis.line(X=np.array([epoch]), Y=np.array([test_iou]), win='mIoU', name='test_mIoU', update="append")
-
-	tmp = pd.Series([
-	    epoch,
-	    train_loss.avg,
-	    train_accuracy.avg,
-	    test_loss.avg,
-	    test_accuracy.avg,
-	    train_iou.item(),
-	    test_iou.item(),
-	], index=['epoch', 'train_loss', 'train_accuracy', 'test_loss', 'test_accuracy', 'train_iou', 'test_iou'])
-	log = log.append(tmp, ignore_index=True)
-	log.to_csv('./finetuning.csv', index=False)
-
 
 	torch.save(net.state_dict(), args.save_path)
